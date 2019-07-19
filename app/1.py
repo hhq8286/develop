de8fbf5b0f369d67bc7246ebd380926cf13a906c
hhq8286 <286542822@qq.com>
2019-07-19 15:01:22 +0800

    Initial commit

---
+++ app/1.py
@@ -0,0 +1,5 @@
+#encoding:utf-8
+# @Time : 2019/7/19 15:01 
+# @Author : huhongqiang
+# @File : 1.py
+print(11111)
---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+#encoding:utf-8
+# @Time : 2019/7/19 15:00 
+# @Author : huhongqiang
+# @File : __init__.py.py 
